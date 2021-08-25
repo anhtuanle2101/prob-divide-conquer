f6676ed889e4e52881ec846c3f632f7b2c58a04a
Tuan Anh Le <anhtuanle2101@gmail.com>
2021-08-25 17:19:44 -0400

    find floor

--- find-floor.js
+++ find-floor.js
@@ -1,5 +1,25 @@
-function findFloor() {
-  
+function findFloor(arr, val) {
+    return binarySearch(arr,val);
+}
+
+function binarySearch(arr, val){
+    let left=0;
+    let right=arr.length-1;
+    while (left<=right){
+        let mid = left+Math.floor((right-left)/2);
+        if (arr[mid]<=val){
+            if (arr[mid+1]<=val){
+                left = mid+1;
+            }else{
+                return arr[mid];
+            }
+        }else if (arr[mid]<val){
+            left = mid+1;
+        }else if (arr[mid]>val){
+            right = mid-1;
+        }
+    }
+    return -1;
 }
 
 module.exports = findFloor
